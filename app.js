7ef685dd790e39b16d68e35da11e262ebb53e845
Kayla Webb <kaylarwebb@gmail.com>
2017-11-08 14:49:31 -0500

    displayAPIData function is recieving data from retrieve function.

--- app.js
+++ app.js
@@ -2,25 +2,32 @@
 
 $(document).ready (function(){
   eventListen();
+  displayAPIData();
   
 });
 
 // endpoint URL for "https://www.googleapis.com/youtube/v3/search"
 
 //retreives API data
-function retrieveApiData(searchTerm){
+function retrieveAPIData(searchTerm){
   const query = {
     q: searchTerm, //grabbed from listener  
     part: 'snippet',
     key: 'AIzaSyBR1d0lPmG_VxIFD-U7iEFl20oyg5WslXU',
   };
+  //how does data know to pass in display results 
   $.getJSON('https://www.googleapis.com/youtube/v3/search', query, function(data) {
-    console.log(data.items);
-   
+    // console.log(data.items);
+   displayAPIData(data);
   });
 }
 
-//function to display the data
+function displayAPIData(data) {
+  const results = data.items;
+
+}
+
+
 //function to append/render retreived data 
 
 //event listener that grabs search term and returns as callback to event handler
@@ -28,8 +35,8 @@ function eventListen() {
   $('.search-form').on('submit', function(e){
     e.preventDefault();
     const searchTerm = $('.search-form').find('.search-query').val();  
-    console.log(searchTerm);
-    retrieveApiData(searchTerm);    
+    // console.log(searchTerm);
+    retrieveAPIData(searchTerm);    
   });
   
 }
