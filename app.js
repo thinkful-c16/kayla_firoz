ce81fc0b27d50ccc3ca2a36a0b1eace1462fdaf5
Firoz Kamdar <firozkamdar@gmail.com>
2017-11-08 09:49:51 -0800

    event functions being created

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
 'use strict';
 
+$(document).ready (function(){
+  eventListen();
+  listenForSearchTerm();
+});
+
 // endpoint URL for "https://www.googleapis.com/youtube/v3/search"
 
-const youtubeURL = 'https://www.googleapis.com/youtube/v3/search';
+//const youtubeURL = 'https://www.googleapis.com/youtube/v3/search';
 
-//function to retreive API data
+// function retrieveApiData(){
+//  $.getJSON('https://www.googleapis.com/youtube/v3/search?', (data) => {
+//   const query = {
+//     q: $(searchTerm), //grabbed from listener  
+//     part: 'snippet',
+//     key: 'AIzaSyBR1d0lPmG_VxIFD-U7iEFl20oyg5WslXU',
+  
+//  }  
+// });
 
+
+//function that tells us what to search for (construct query object)
 //function to render results to results <div>
 
-youtube api documentation:
+//event listener that grabs search term and returns as callback to event handler
+function eventListen() {
+  $('.search-button').on('click', function(e){
+    e.preventDefault();
+    
+    
+  });
+}
+//event handler grabs search term and passes it to function to display the result
 
-https://developers.google.com/youtube/v3/code_samples/javascript
+function listenForSearchTerm(){
+  $('.search-query').on('submit', function(e){
+    e.preventDefault();
+    const searchTerm = $('.search-query').val();  
+    console.log(searchTerm);
+    
+  });
+}
+
+
+
+//youtube api documentation:
+
+//https://developers.google.com/youtube/v3/code_samples/javascript
+
+// key AIzaSyBR1d0lPmG_VxIFD-U7iEFl20oyg5WslXU
