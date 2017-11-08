0b73f45aa0e9e998faed29579b0f063c769039e1
Firoz Kamdar <firozkamdar@gmail.com>
2017-11-08 08:29:57 -0800

    files moved to correct repo

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// endpoint URL for "https://www.googleapis.com/youtube/v3/search"
+
+const youtubeURL = 'https://www.googleapis.com/youtube/v3/search';
+
+//function to retreive API data
+
+//function to render results to results <div>
+
+youtube api documentation:
+
+https://developers.google.com/youtube/v3/code_samples/javascript
