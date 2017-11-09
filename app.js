9c2e1b1e469e6c0cbdcfcbdd6fe595a14d342f31
Kayla Webb <kaylarwebb@gmail.com>
2017-11-08 20:45:56 -0500

    included the itemIndex on our results.

--- app.js
+++ app.js
@@ -11,24 +11,26 @@ $(document).ready (function(){
 //retreives API data
 function retrieveAPIData(searchTerm){
   const query = {
-    q: searchTerm, //grabbed from listener  
+    q: searchTerm, //grabbed from event istener  
     part: 'snippet',
     key: 'AIzaSyBR1d0lPmG_VxIFD-U7iEFl20oyg5WslXU',
     maxResults: 20
   };
-  //how does data know to pass in display results 
   $.getJSON('https://www.googleapis.com/youtube/v3/search', query, function(data) {
     // console.log(data.items);
+    //pass our callback function data from the API to the function that needs it
     displayAPIData(data);
   });
 }
 
 function displayAPIData(data) {
   //create an object to attach to html
+  //map over the data recieved from the API and grab the properties we need
   const results = data.items.map(function(value, index) {
     const videoID = value.id['videoId']; 
     const medThumbURL = value.snippet.thumbnails.medium.url;
     const vidTitle = value.snippet.title;
+    //return our relevant data into an object array
     return {
       videoID: videoID,
       medThumbURL: medThumbURL,
@@ -36,31 +38,36 @@ function displayAPIData(data) {
     };
   });
   console.log(results);
-  generateTemplate(results);
+  //pass our object to the template
+  generateResultsTemplate(results);
 }
 
 //transforms data to html
-function generateTemplate(results){
+function generateResultsTemplate(results){
+  //map over the object array since it needs to be transformed
+  //put the output of the array in the template dynamically
   let resultsMapped = results.map(function(item, index){
     return `
-    <li>
-      <div>${item.vidTitle}</div>
+    <li class='js-item-index' data-item-index=${index}>
+      <div class='js-video-title'>${item.vidTitle}</div>
       <span>${item.videoID}</span>
       <div><img src=${item.medThumbURL} alt="alt text here">
       </div>
     </li>
     `;
   });
+  //pass the results to the render function
   renderTemplate(resultsMapped);
 }
 
 
 function renderTemplate(resultsMapped){
+  //join the results into one string 
   resultsMapped.join('');
+  //append the string to the DOM
   $('.search-results-ul').html(resultsMapped);
 }
 
-//function to append/render retreived data 
 
 //event listener that grabs search term and returns as callback to event handler
 function eventListen() {
@@ -68,6 +75,8 @@ function eventListen() {
     e.preventDefault();
     const searchTerm = $('.search-form').find('.search-query').val();  
     // console.log(searchTerm);
+
+    //pass the value obtained from the listener to the API call
     retrieveAPIData(searchTerm);    
   });
   
